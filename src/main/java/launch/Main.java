ba87c08e1e3952689bf5e199f180047a7d03da25
ninojairo <nino.jairo@gmail.com>
2018-08-11 12:12:44 -0500

    heroku deploy 2

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.itfactoria.ejemploservlets;
+package launch;
 
 import java.io.File;
 import org.apache.catalina.WebResourceRoot;
